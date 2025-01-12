04b411edcd6892fd39eca90e1060eb2a6de0d556
Minigrim0 <grimauflorent@gmail.com>
2025-01-12 22:33:37 +0100

    added parser testing

---
+++ src/virtual_machine/parser.rs
@@ -0,0 +1,108 @@
+use super::{Instruction, Instructions, Registers, MemoryMappedProperties};
+
+fn parse_instr<S: AsRef<str>>(instr: S) -> Instructions {
+    match instr.as_ref() {
+        "mov" => Instructions::MOV,
+        "movi" => Instructions::MOVI,
+        "store" => Instructions::STORE,
+        "storei" => Instructions::STOREI,
+        "load" => Instructions::LOAD,
+        "loadi" => Instructions::LOADI,
+        _ => {
+            println!("Unknown instruction: {}", instr.as_ref());
+            Instructions::NOP
+        }
+    }
+}
+
+fn parse_operand<S: AsRef<str>>(operand: S) -> Result<i32, ()> {
+    match operand.as_ref().chars().next() {
+        Some('$') => {
+            println!("Operand is a special variable");
+            match operand.as_ref().chars().skip(1).collect::<String>().as_str() {
+                "VelocityX" => Ok(MemoryMappedProperties::VelocityX as i32),
+                "VelocityY" => Ok(MemoryMappedProperties::VelocityY as i32),
+                "Moment" => Ok(MemoryMappedProperties::Moment as i32),
+                "Rotation" => Ok(MemoryMappedProperties::Rotation as i32),
+                "PositionX" => Ok(MemoryMappedProperties::PositionX as i32),
+                "PositionY" => Ok(MemoryMappedProperties::PositionY as i32),
+                var => {
+                    println!("Unknown variable: {}", var);
+                    Err(())
+                }
+            }
+        },
+        Some('#') => {
+            println!("Operand is a litteral");
+            operand.as_ref().chars().skip(1).collect::<String>().parse::<i32>().map_err(|e| {
+                println!("Unable to parse int : {}", e.to_string());
+            })
+        },
+        Some('\'') => {
+            println!("Operand is a register");
+            match operand.as_ref().chars().skip(1).collect::<String>().as_str() {
+                "GPA" => Ok(Registers::GPA as i32),
+                "GPB" => Ok(Registers::GPB as i32),
+                "GPC" => Ok(Registers::GPC as i32),
+                "GPD" => Ok(Registers::GPD as i32),
+                "FPA" => Ok(Registers::FPA as i32),
+                "FPB" => Ok(Registers::FPB as i32),
+                "FPC" => Ok(Registers::FPC as i32),
+                "FPD" => Ok(Registers::FPD as i32),
+                reg => {
+                    println!("Unknown register: {}", reg);
+                    Err(())
+                }
+            }
+        },
+        Some(_) => {
+            operand.as_ref().chars().skip(1).collect::<String>().parse::<i32>().map_err(|e| {
+                println!("Unable to parse int : {}", e.to_string());
+            })
+        },
+        None => {
+            println!("No operand to parse !");
+            Err(())
+        }
+    }
+}
+
+pub fn parse<S: AsRef<str>>(text: S) -> Vec<Instruction> {
+    let mut instructions = vec![];
+    'main_loop: for line in text.as_ref().split("\n") {
+        println!("Working on line: {}", line);
+        if line.chars().next() == Some(';') {
+            println!("Comment line, skipping");
+            continue;
+        }
+
+        let splitted_line = line.split(" ").collect::<Vec<&str>>();
+        let mut splitted_line = splitted_line.iter();
+        let instruction = Instruction {
+            opcode: match splitted_line.next() {
+                Some(instr) => parse_instr(instr),
+                None => {
+                    println!("No intruction found for line '{}'", line);
+                    break 'main_loop;
+                }
+            },
+            operand_1: match splitted_line.next() {
+                Some(op) => match parse_operand(op) {
+                    Ok(op) => op,
+                    Err(()) => {
+                        println!("Error in program code");
+                        break 'main_loop;
+                    }
+                },
+                None => {
+                    println!("No intruction found for line '{}'", line);
+                    break 'main_loop;
+                }
+            },
+            operand_2: splitted_line.next().and_then(|operand| parse_operand(operand).ok())
+        };
+        instructions.push(instruction);
+    }
+
+    instructions
+}
---
+++ src/virtual_machine/tests/test_parser.rs
@@ -0,0 +1,39 @@
+use std::fs;
+
+use super::super::parser::parse;
+use super::super::{Instruction, Instructions, Registers, MemoryMappedProperties};
+
+#[test]
+fn test_parser() {
+    let text = fs::read_to_string("assets/programs/turn.csasm");
+    assert!(text.is_ok(), "Unable to read input file: {}", text.err().unwrap().to_string());
+    let instructions = parse(text.unwrap());
+
+    let expected = vec![
+        Instruction {
+            opcode: Instructions::MOVI,
+            operand_1: (Registers::GPA as i32),
+            operand_2: Some(MemoryMappedProperties::VelocityY as i32),
+        },
+        Instruction {
+            opcode: Instructions::MOVI,
+            operand_1: (Registers::GPB as i32),
+            operand_2: Some(MemoryMappedProperties::Moment as i32),
+        },
+        Instruction {
+            opcode: Instructions::STOREI,
+            operand_1: (Registers::GPA as i32),
+            operand_2: Some(100),
+        },
+        Instruction {
+            opcode: Instructions::STOREI,
+            operand_1: (Registers::GPB as i32),
+            operand_2: Some(-100),
+        }
+    ];
+
+    assert_eq!(instructions.len(), expected.len(), "Instruction set does not have the correct size");
+    for (i1, i2) in instructions.iter().zip(expected.iter()) {
+        assert_eq!(i1, i2);
+    }
+}
