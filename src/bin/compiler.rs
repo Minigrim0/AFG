788b4fec13e03886570fc67efc3f658436af6d59
Minigrim0 <grimauflorent@gmail.com>
2025-01-20 23:00:18 +0100

    added ability to save intermediate data + pseudo asm generation

--- src/bin/compiler.rs
+++ src/bin/compiler.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use std::fs;
 
-use csai::lang::{TokenStream, AST, analyze, SemanticError};
+use csai::lang::{analyze, PASMProgram, SemanticError, TokenStream, AST};
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -17,17 +17,30 @@ struct Args {
 fn main() -> Result<(), String> {
     let args = Args::parse();
 
-    let text = fs::read_to_string(args.input).map_err(|e| e.to_string())?;
+    let text = fs::read_to_string(&args.input).map_err(|e| e.to_string())?;
 
     let mut tokens = TokenStream::lex(text);
-    let program = AST::parse(&mut tokens)?;
+    if args.save_intermediate {
+        let token_output = args.input.clone() + ".tokens";
+        fs::write(&token_output, format!("{}", tokens)).map_err(|e| e.to_string())?;
+    }
 
-    println!("{}", program);
+    let program = AST::parse(&mut tokens)?;
+    if args.save_intermediate {
+        let ast_output = args.input.clone() + ".ast";
+        fs::write(&ast_output, format!("{}", program)).map_err(|e| e.to_string())?;
+    }
 
     analyze(&program).map_err(|e| match e {
         SemanticError::UnknownVariable(e) => e,
         SemanticError::InvalidOperation(e) => e,
     })?;
+
+    let pasm = PASMProgram::parse(program)?;
+    if args.save_intermediate {
+        let pasm_output = args.input.clone() + ".pasm";
+        fs::write(&pasm_output, format!("{}", pasm)).map_err(|e| e.to_string())?;
+    }
 
     Ok(())
 }
